df6124c9d8d82260de393be9a9453b8bdbcbba69
Mahir Arcangel Arcia Acosta <87144202+Arcangel18@users.noreply.github.com>
2023-06-27 11:59:43 -0500

    Add files via upload

---
+++ Suma.java
@@ -0,0 +1,13 @@
+public class Suma {
+    public static void main(String[] args) {
+        int uno=1, dos=2, tres=3, resultado=0;
+        Sumados(uno,dos ,tres,resultado);
+
+         
+    }
+
+        void Sumados(int uno,int dos, int tres,int resultado){
+
+             resultado=uno+dos+tres;
+        }
+}
