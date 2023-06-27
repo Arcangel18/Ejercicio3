865a56e2745e6d323c06b177daa0dbbc65fadafe
Mahir Arcangel Arcia Acosta <87144202+Arcangel18@users.noreply.github.com>
2023-06-27 11:53:44 -0500

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,5 +1,16 @@
-public class App {
+public class Car {
+    static int puertas=0;
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Worldqdmkmckwc");
+        
+        Car miCoche = new Car();
+        miCoche.sumarpuertas();
+        System.out.println("las puertas que tienes son");
+        System.out.println(puertas);
+
     }
+
+    void sumarpuertas(){
+        puertas++;
+    } 
 }
+
